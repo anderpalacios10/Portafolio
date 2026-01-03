622d1a510ef0caa5fccaee2c2cec1496598c75a6
Anderson Palacios <andersonrpalacios@gmail.com>
2026-01-02 20:20:38 -0500

    netifly pubicar

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-
+import { wayfinder } from '@laravel/vite-plugin-wayfinder';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
@@ -8,17 +8,13 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
-          
+            ssr: 'resources/js/ssr.ts',
             refresh: true,
-            // 1. Desactiva la detección de PHP aquí
-            detectPhp: false, 
         }),
         tailwindcss(),
-        // 2. Comenta o elimina Wayfinder temporalmente si sigue dando error, 
-        // ya que suele requerir PHP para generar las rutas.
-        // wayfinder({
-        //    formVariants: true,
-        // }),
+        wayfinder({
+            formVariants: true,
+        }),
         vue({
             template: {
                 transformAssetUrls: {
